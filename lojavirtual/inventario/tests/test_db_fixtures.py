754f4f6345d1082c6bbacb1b990a45c0503ae0d4
filipevsco <filipev@me.com>
2022-09-17 17:51:27 -0300

    test produto 1

--- lojavirtual/inventario/tests/test_db_fixtures.py
+++ lojavirtual/inventario/tests/test_db_fixtures.py
@@ -1,4 +1,8 @@
 from cgitb import reset
+from pydoc import describe
+from time import strftime
+from unittest import result
+from venv import create
 
 import pytest
 from lojavirtual.inventario import models
@@ -9,6 +13,7 @@ from lojavirtual.inventario import models
     "id, name, slug, is_active",
     [
         (1, "fashion", "fashion", 1),
+        (18,, on", 1),
         (18, "trainers", "trainers", 1),
         (35, "baseball", "baseball", 1),
     ],
@@ -37,3 +42,41 @@ def test_inventorio_db_categoria_inserir_dados(
 
     assert result.slug == slug
     assert result.is_active == is_active
+
+
+@pytest.mark.dbfixture
+@pytest.mark.parametrize(
+    "id, web_id, name, slug, decription, is_active, created_at, updated_at",
+    [
+        (
+            1, 
+            "45425810", 
+            "widstar running sneakers", 
+            "widstar-running-sneakers",
+            "Lorem ipsu, dolor sit amet, consectetur adpiscing elit. Pront porta, eros vel solicita.",
+            1, 
+            "2021-09-04 22:14:18", 
+            "2021-09-04 22:14:18",
+        ),
+                (
+            8616, 
+            "45434425", 
+            "impact puse dance shoe", 
+            "impact-puse-dance-shoe",
+            "Loremo nsectetur adpiscing elit. Pront porta, eros vel solicita. asre dois gol abacaxi",
+            1, 
+            "2021-09-04 22:14:18", 
+            "2021-09-04 22:14:18",
+        ),
+    ],
+)
+def test_inventario_db_produto_dbfixture(db, django_db_setup, id, web_id, name, slug, decription, is_active, created_at, updated_at):
+    result = modes.Produto.objects.get(id=id)
+    result_create_at = result.created_at.strftime("%Y-%m-%d %H:%M:%S")
+    result_updated_at = result.updated_at.strftime("%Y-%m-%d %H:%M:%S")
+    assert result.web_id == web_id
+    assert result.slug == slug
+    assert result.description == description
+    assert result.is_active == is_active
+    assert result.created_at == created_at
+    assert result.updated_at == updated_at
