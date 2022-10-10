ca9d663875c7fd412db2003f50f79b7ba191dfc6
filipevsco <filipev@me.com>
2022-10-10 12:27:03 -0300

    model product attribute

--- lojavirtual/inventory/tests/test_db_fixtures.py
+++ lojavirtual/inventory/tests/test_db_fixtures.py
@@ -1,4 +1,4 @@
-from venv import create
+from unicodedata import name
 
 import pytest
 from django.db import IntegrityError
@@ -320,3 +320,38 @@ def test_inventory_db_stock_insert_data(db, stock_factory):
     assert new_stock.product_inventory.sku == "123456789"
     assert new_stock.units == 2
     assert new_stock.units_sold == 100
+
+
+@pytest.mark.dbfixture
+@pytest.mark.parametrize(
+    "id, name, description",
+    [
+        (1, "men shoe size", "men shoe size"),
+    ],
+)
+def test_inventory_db_porduct_attribute_dataset(
+    db,
+    db_fiture_setup,
+    id,
+    name,
+    description,
+):
+    result = ProductAttribute.objects.get(id=id)
+    assert result.name == name
+    assert result.description == description
+
+
+def test_inventory_db_product_attribute_insert_data(
+    db, product_atribute_factory
+):
+    new_attribute = product_atribute_factory.create()
+    assert new_attribute.name == "atribute_name_0"
+    assert new_attribute.description == "description_0"
+
+
+def test_inventory_db_product_attribute_uniqueness_integrity(
+    db, product_attribute_factory
+):
+    product_attribute_factory.create("not_unique")
+    with pytest.raises(IntegrityError):
+        product_attribute_factory.create(name="not_unique")
