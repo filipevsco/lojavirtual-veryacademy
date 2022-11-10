669bed0f8c17e320601351fd2a7f5e98312ccd88
filipevsco <filipev@me.com>
2022-11-10 20:02:14 -0300

    retrieve

--- lojavirtual/drf/views.py
+++ lojavirtual/drf/views.py
@@ -1,4 +1,5 @@
 from rest_framework import mixins, permissions, viewsets
+from rest_framework.response import Response
 
 from lojavirtual.drf.serializer import AllProducts
 from lojavirtual.inventory.models import Product
@@ -14,3 +15,8 @@ class AllProductsViewset(
     serializer_class = AllProducts
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     lookup_field = "slug"
+
+    def retrieve(self, request, slug=None):
+        queryset = Product.objects.filter(category__slug=slug)[:10]
+        serializer = AllProducts(queryset, many=True)
+        return Response(serializer.data)
